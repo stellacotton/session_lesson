f3c7f02fd83de609fd897cfcdf13263476ae4461
Stella Cotton <stella@stellacotton.com>
2013-11-14 09:09:22 -0800

    add game

--- app.py
+++ app.py
@@ -90,6 +90,10 @@ def logout():
     session.clear()
     return redirect(url_for("index"))
 
+@app.route("/game")
+def game():
+    return render_template("game.html")
+
 
 if __name__ == "__main__":
     app.run(debug = True)
